b1d738d1e44a04eaff43c228eff49973005e7986
Bavin Edwards <65621465+zerico007@users.noreply.github.com>
2022-07-01 21:50:13 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<template>
+  <div class="app-container">
+    <Navbar />
+  </div>
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+.app-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #42022e;
+  width: 100vw;
+  padding: 1rem;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<template>
+  <nav>
+    <h2>Foodie</h2>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.25rem 2rem;
+  background-color: var(--main-background);
+  width: 100%;
+  height: 4.5rem;
+  font-size: 1.2rem;
+  font-weight: bold;
+  z-index: 1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  box-sizing: border-box;
+}
+
+h2 {
+  color: var(--main-white);
+  cursor: pointer;
+  font-family: "Dancing Script", cursive;
+  font-size: 40px;
+}
+</style>
